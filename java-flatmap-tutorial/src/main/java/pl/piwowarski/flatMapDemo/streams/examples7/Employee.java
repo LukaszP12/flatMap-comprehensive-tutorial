1d6e2b18d185b305232b23a80d68449e5235df05
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-10-24 13:42:29 +0200

    ✅ Example 5: Company → Employees → Skills (Flatten Strings)

---
+++ java-flatmap-tutorial/src/main/java/pl/piwowarski/flatMapDemo/streams/examples7/Employee.java
@@ -0,0 +1,12 @@
+package pl.piwowarski.flatMapDemo.streams.examples7;
+
+import java.util.List;
+
+class Employee {
+    String name;
+    List<String> skills;
+    Employee(String name, List<String> skills) {
+        this.name = name;
+        this.skills = skills;
+    }
+}
