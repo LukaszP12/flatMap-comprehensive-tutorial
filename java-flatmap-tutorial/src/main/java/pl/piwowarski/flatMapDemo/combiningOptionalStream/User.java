d16f9947157cd96d71d96b13f9073f5836f4fecd
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-10-25 17:51:50 +0200

    ✔️ Extract all existing emails ✔️ Avoid null checks ✔️ Use Optional.flatMap() + Streams cleanly

---
+++ java-flatmap-tutorial/src/main/java/pl/piwowarski/flatMapDemo/combiningOptionalStream/User.java
@@ -0,0 +1,9 @@
+package pl.piwowarski.flatMapDemo.combiningOptionalStream;
+
+import java.util.Optional;
+
+class User {
+    private Contact contact;
+    public User(Contact contact) { this.contact = contact; }
+    public Optional<Contact> getContact() { return Optional.ofNullable(contact); }
+}
