fb23e2330755713ff46f403a89cf98bef0c9eade
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-10-25 17:14:22 +0200

    ✅ Add Tree flattening + sum method

---
+++ java-flatmap-tutorial/src/main/java/pl/piwowarski/flatMapDemo/streams/TreeNode/TreeDemo.java
@@ -0,0 +1,41 @@
+package pl.piwowarski.flatMapDemo.streams.TreeNode;
+
+import java.util.stream.Stream;
+
+class TreeDemo {
+    public static void main(String[] args) {
+        // Build tree
+        TreeNode root = new TreeNode(1);
+        TreeNode childA = new TreeNode(2);
+        TreeNode childB = new TreeNode(3);
+        TreeNode childC = new TreeNode(4);
+        TreeNode childD = new TreeNode(5);
+
+        root.addChild(childA);
+        root.addChild(childB);
+        childA.addChild(childC);
+        childB.addChild(childD);
+
+        // Calculate sum
+        TreeDemo demo = new TreeDemo();
+        int sum = demo.sumValues(root);
+        System.out.println("Sum of all tree values: " + sum);
+    }
+
+    public Stream<TreeNode> flatten(TreeNode node) {
+        if (node == null) {
+            Stream.empty();
+        }
+        return Stream.concat(
+                Stream.of(node),
+                node.getChildren().stream().flatMap(this::flatten)
+        );
+    }
+
+    // sum all values using flatten()
+    public int sumValues(TreeNode root) {
+        return flatten(root)
+                .mapToInt(TreeNode::getValue)
+                .sum();
+    }
+}
